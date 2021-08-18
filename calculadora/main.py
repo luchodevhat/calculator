938585638b398f95197d34f856efd7757a18f2b7
Luis Alejandro <alfaroalejandro597@gmail.com>
2021-08-18 16:36:41 -0600

    avance miercoles

--- calculadora/main.py
+++ calculadora/main.py
@@ -1,61 +1,49 @@
 from tkinter import *
 from math import *
 
+
+
 class Aplicacion_menu:
-    def __init__(self, comas=0, input_text="", width=2, height=1):
-        self.comas = comas
-        self.input_text = input_text
-        self.width = width
-        self.height = height
+    def __init__(self, n_1="", r="", texto=Entry ):
+        self.n_1 = n_1
+        self.r = r
+        self.texto = texto
 
-    
-    def clear(): 
-        pass
-
-    def digit():
-        pass
-
-    def coma():
-        pass
-
-
-    
-    def menu_start(self):
-        # Raiz
+     # Raiz
         root = Tk()
         root.title("Calculadora polinomios")
         root.resizable(0,0)
 
         # FramePrincipal
-        frame = Frame(root, width=420, height=580)
+        frame = Frame(root, width=420, height=280)
         frame.pack()
         frame.config(cursor="plus", bg="gray20", bd=25)
 
         #Entry
-        pantalla = Entry(frame, width=40)
-        pantalla.place(x=65, y=50)
-        pantalla.config(bg="peachpuff", justify="center", state="disabled", relief="sunken")
+        texto = Entry(frame, width=40, textvariable=self.r)
+        texto.place(x=65, y=50)
+        texto.config(bg="peachpuff", justify="center", state="disabled", relief="sunken")
  
 
         #Etiquetas
         label_1 = Label(frame, text="Bienvenido a la calculadora PYTHON")
-        label_1.place(x=90, y=505)
+        label_1.place(x=90, y=1)
         label_1.config(bg="peachpuff", relief="groove")
 
         #Botones
-        button_0 = Button(frame, text= 0, width=2, height=1)
+        button_0 = Button(frame, text= 0, width=2, height=1, textvariable=self.n_1, command=lambda: self.click_num(0))
         button_0.place(x=60, y=100)
         button_0.config(bg="gray50")
 
-        button_1 = Button(frame, text= 1, width=2, height=1)
+        button_1 = Button(frame, text= 1, width=2, height=1, command=lambda: self.click_num(1))
         button_1.place(x=90, y=100)
         button_1.config(bg="gray50")
 
-        button_2 = Button(frame, text= 2, width=2, height=1)
+        button_2 = Button(frame, text= 2, width=2, height=1, command=lambda: self.click_num(2))
         button_2.place(x=120, y=100)
         button_2.config(bg="gray50")
 
-        button_3 = Button(frame, text= 3, width=2, height=1)
+        button_3 = Button(frame, text= 3, width=2, height=1, command=lambda: self.click_num(3))
         button_3.place(x=150, y=100)
         button_3.config(bg="gray50")
 
@@ -69,51 +57,51 @@ class Aplicacion_menu:
         button_c.config(bg="red")
 
 
-        button_4 = Button(frame, text= 4, width=2, height=1)
+        button_4 = Button(frame, text= 4, width=2, height=1, command=lambda: self.click_num(4))
         button_4.place(x=60, y=130)
         button_4.config(bg="gray50")
 
-        button_5 = Button(frame, text= 5, width=2, height=1)
+        button_5 = Button(frame, text= 5, width=2, height=1, command=lambda: self.click_num(5))
         button_5.place(x=90, y=130)
         button_5.config(bg="gray50")
 
-        button_6 = Button(frame, text= 6, width=2, height=1)
+        button_6 = Button(frame, text= 6, width=2, height=1, command=lambda: self.click_num(6))
         button_6.place(x=120, y=130)
         button_6.config(bg="gray50")
 
-        button_7 = Button(frame, text= 7, width=2, height=1)
+        button_7 = Button(frame, text= 7, width=2, height=1, command=lambda: self.click_num(7))
         button_7.place(x=150, y=130)
         button_7.config(bg="gray50")
 
-        button_8 = Button(frame, text= 8, width=2, height=1)
+        button_8 = Button(frame, text= 8, width=2, height=1, command=lambda: self.click_num(8))
         button_8.place(x=180, y=130)
         button_8.config(bg="gray50")
 
-        button_9 = Button(frame, text= 9, width=2, height=1)
+        button_9 = Button(frame, text= 9, width=2, height=1, command=lambda: self.click_num(9))
         button_9.place(x=210, y=130)
         button_9.config(bg="gray50")
 
-        button_sum = Button(frame, text= "+", width=2, height=1)
+        button_sum = Button(frame, text= "+", width=2, height=1, command=lambda: self.click_num("+"))
         button_sum.place(x=60, y=160)
         button_sum.config(bg="gray50")
 
-        button_subtraction = Button(frame, text= "-", width=2, height=1)
+        button_subtraction = Button(frame, text= "-", width=2, height=1, command=lambda: self.click_num("-"))
         button_subtraction.place(x=90, y=160)
         button_subtraction.config(bg="gray50")
 
-        button_division = Button(frame, text= "/", width=2, height=1)
+        button_division = Button(frame, text= "/", width=2, height=1, command=lambda: self.click_num("/"))
         button_division.place(x=120, y=160)
         button_division.config(bg="gray50")
 
-        button_multiplication = Button(frame, text= "*", width=2, height=1)
+        button_multiplication = Button(frame, text= "*", width=2, height=1, command=lambda: self.click_num("*"))
         button_multiplication.place(x=150, y=160)
         button_multiplication.config(bg="gray50")
 
-        button_square_root = Button(frame, text= "√", width=2, height=1)
+        button_square_root = Button(frame, text= "√", width=2, height=1, command=lambda: self.click_num("√"))
         button_square_root.place(x=180, y=160)
         button_square_root.config(bg="gray50")
 
-        button_rest = Button(frame, text= "%", width=2, height=1)
+        button_rest = Button(frame, text= "%", width=2, height=1, command=lambda: self.click_num("%"))
         button_rest.place(x=210, y=160)
         button_rest.config(bg="gray50")
 
@@ -123,6 +111,12 @@ class Aplicacion_menu:
 
         root.mainloop()
         
+    def click_num(self, n_1):
+        self.texto.insert(0, str(n_1))
+
+
+
+       
 
 
 
